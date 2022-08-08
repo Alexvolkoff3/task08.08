111845bb0dfe6d78552f7b50bd5b7777f1de6cf1
prombotpy <alexvolkoff5@gmail.com>
2022-08-08 21:30:20 +0300

    wow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import peewee
+import telebot
+from config import token
+from telebot.types import Message
 
-db = peewee.sqlite3('data.db')
+bot = telebot.TeleBot(token)
 
-class User(peewee.Model):
-    pass
+@bot.message_handler()
+def echo(message: Message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling(skip_pending=True)
